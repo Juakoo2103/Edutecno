9402039f87490ec632afca2c7b5250870bf3f294
Juakoo2103 <77761177+Juakoo2103@users.noreply.github.com>
2023-04-22 13:31:52 -0400

    practica personal

---
+++ Modulo 3/5-S5/practica-personal.py
@@ -0,0 +1,23 @@
+"""Práctica Control de Flujo 2 (IF)
+
+Las leyes de un país establecen que un adulto puede conducir si tiene licencia para hacerlo, y para optar por una licencia para conducir, debe de tener 18 años o más.
+
+Crea una estructura condicional para verificar si una persona de 16 años sin licencia puede conducir, y muestra el resultado que corresponda en pantalla:
+
+"Puedes conducir"
+
+"No puedes conducir aún. Debes tener 18 años y contar con una licencia"
+
+"No puedes conducir. Necesitas contar con una licencia"""
+
+edad = int(input("Ingresa tu edad: "))
+tiene_licencia = input(
+    "¿Tienes licencia de conducir? (Sí/No): ").lower() == "sí"
+
+if edad >= 18:
+    if tiene_licencia:
+        print("Puedes conducir")
+    else:
+        print("No puedes conducir. Necesitas contar con una licencia")
+else:
+    print("No puedes conducir aún. Debes tener 18 años y contar con una licencia")
