b53712e6b66c824f69bc465d49e86644976cdc28
Juakoo2103 <77761177+Juakoo2103@users.noreply.github.com>
2023-04-22 13:08:16 -0400

    Drilling for

---
+++ Modulo 3/5-S5/drilling.py
@@ -0,0 +1,20 @@
+# eliminar letras de un string e imprimir en pantalla las consonantes restantes y su posicion dentro del string
+
+palabra = "paralelepipedo"
+vocales = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
+
+for letra in palabra:
+    if letra not in vocales:
+        print(letra)
+
+print("-------------------------------------------------------")
+
+palabra = "paralelepipedo"
+consonantes = ""
+
+for i in range(len(palabra)):
+    if palabra[i] not in "aeiouAEIOU":
+        consonantes += palabra[i]
+        print(f"la latra {palabra[i]} se encuentra en la posicion {i}")
+
+print("-------------------------------------------------------")
