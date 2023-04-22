fd2c6388fd291471f0664ccbeabda78e006eb4dd
Juakoo2103 <77761177+Juakoo2103@users.noreply.github.com>
2023-04-22 02:18:55 -0400

    cambios en el formato para mas placer xd

--- Modulo 3/4-S4/drilling.py
+++ Modulo 3/4-S4/drilling.py
@@ -4,7 +4,7 @@ Para ello, generamos un diccionario para cada una de las personas con los datos 
 y luego los almacenaremos dentro de una lista. Finalmente, imprimiremos en pantalla la lista
 """
 
-#los diccionarios, colecciones de datos formados por clave y valor
+# los diccionarios, colecciones de datos formados por clave y valor
 
 person1 = {
     "nombre": "Juan",
@@ -29,12 +29,13 @@ lista = [person1, person2, person3]
 print(lista)
 
 pacientes = {
-    'paciente1': {'name': "Joaquin" ,'edad': 75, 'peso': 75},
-    'paciente2': {'name': "Ana" ,'edad': 19, 'peso': 75},
-    'paciente3': {'name': "Cristobal" ,'edad': 21, 'peso': 75},
-    'paciente4': {'name': "Luisa" ,'edad': 22, 'peso': 75}
+    'paciente1': {'name': "Joaquin", 'edad': 75, 'peso': 75},
+    'paciente2': {'name': "Ana", 'edad': 19, 'peso': 75},
+    'paciente3': {'name': "Cristobal", 'edad': 21, 'peso': 75},
+    'paciente4': {'name': "Luisa", 'edad': 22, 'peso': 75}
 }
 
 for key, value in pacientes.items():
     print(f"Paciente {key}:")
-    print(f"Nombre: {value['name']}\nEdad: {value['edad']}\nPeso: {value['peso']}\n")
+    print(
+        f"Nombre: {value['name']}\nEdad: {value['edad']}\nPeso: {value['peso']}\n")
