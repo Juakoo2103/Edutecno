94fdd113cd526f2cad9df7137061bcfe14e7203a
Juakoo2103 <77761177+Juakoo2103@users.noreply.github.com>
2023-04-24 21:40:42 -0400

    ejercicios dudas

--- Modulo 3/5-S5/ejercicios-24.py
+++ Modulo 3/5-S5/ejercicios-24.py
@@ -16,11 +16,52 @@
 # R2 = float(input("Ingrese la resistencia 2: "))
 # R3 = float(input("Ingrese la resistencia 3: "))
 
-# Rt = 1/((1/R1)+(1/R2)+(1/R3))
+# r1 = abs(R1)
+# r2 = abs(R2)
+# r3 = abs(R3)
+
+# print("aca con abs se pasa a valores absolutos")
+# print(f"la resistencia de r1 es ", r1)
+# print(f"la resistencia de r2 es ", r2)
+# print(f"la resistencia de r3 es ", r3)
+# Rt = 1/((1/r1)+(1/r2)+(1/r3))
+
 
 # print("La resistencia total del circuito es: {:.2f}".format(Rt))
 
 # print("======================================================================")
+
+
+while True:
+    R1 = input("Ingrese la resistencia 1: ")
+    if R1.isdigit():
+        R1 = float(R1)
+        r1 = abs(R1)
+        break
+    else:
+        print("Error: Debe ingresar solo números. Intente de nuevo.")
+
+while True:
+    R2 = input("Ingrese la resistencia 2: ")
+    if R2.isdigit():
+        R2 = float(R2)
+        r2 = abs(R2)
+        break
+    else:
+        print("Error: Debe ingresar solo números. Intente de nuevo.")
+
+while True:
+    R3 = input("Ingrese la resistencia 3: ")
+    if R3.isdigit():
+        R3 = float(R3)
+        r3 = abs(R3)
+        break
+    else:
+        print("Error: Debe ingresar solo números. Intente de nuevo.")
+
+Rt = 1 / ((1/r1) + (1/r2) + (1/r3))
+
+print("La resistencia total del circuito es: {:.2f}".format(Rt))
 
 
 # Realizar el calculo de la hipotenusa requiriendo el ingreso del cateto a y cateto b por parte del usuario en consola
@@ -68,30 +109,30 @@
 # Realizar la ejecucion de un menu utilizando el lenguaje python, donde se le indiquen varias opciones a seleccionar por el usuario y una opcion para salir del
 # menu utulizar ciclos y estructuras condicionales
 
-mensaje = """ Menu principal 
+# mensaje = """ Menu principal
 
-[1] Entrar en menu principal
+# [1] Entrar en menu principal
 
-[2] Novedades
+# [2] Novedades
 
-[3] Opciones
+# [3] Opciones
 
-[4] Salir """
-opcion = ""
-while opcion != "4":
-    print(mensaje)
+# [4] Salir """
+# opcion = ""
+# while opcion != "4":
+#     print(mensaje)
 
-    opcion = input("Elija una opción: ")
-    if opcion == "1":
-        print("Entrando en menu principal")
-    elif opcion == "2":
-        print("Novedades")
-    elif opcion == "3":
-        print("Opciones")
-    elif opcion == "4":
-        print("Salir")
-    else:
-        print("Opción invalida")
+#     opcion = input("Elija una opción: ")
+#     if opcion == "1":
+#         print("Entrando en menu principal")
+#     elif opcion == "2":
+#         print("Novedades")
+#     elif opcion == "3":
+#         print("Opciones")
+#     elif opcion == "4":
+#         print("Salir")
+#     else:
+#         print("Opción invalida")
 
 
-print("======================================================================")
+# print("======================================================================")
