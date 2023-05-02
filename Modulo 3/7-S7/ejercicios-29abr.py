64db9973b0d64d0efa1645c78fc6f2e240159170
Juakoo2103 <77761177+Juakoo2103@users.noreply.github.com>
2023-05-02 19:32:43 -0400

    ejercicios 29 abril

---
+++ Modulo 3/7-S7/ejercicios-29abr.py
@@ -0,0 +1,21 @@
+lista = [1] * 5000
+lista.insert(2300, "arroz")
+
+print(lista)
+
+# Sort es un metodo que modifica la actual lista de datos
+# Sorted es una funcion que debes crear una nueva lista para que se guarden los datos
+
+numero = [1, 2, 3, 4, 5, 6, 7, 8]
+
+numero.sort(reverse=True)
+print(numero) #aca se asigna un nuevo valor a la misma lista
+
+
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8]
+numerosOrdenados = sorted(numeros, reverse=True)
+print(numerosOrdenados)
+
+
+
