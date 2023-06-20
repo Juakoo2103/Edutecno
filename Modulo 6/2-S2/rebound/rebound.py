66eb3ba583f121f0f35ab3e44af9b2ec6f624fef
Juakoo2103 <77761177+Juakoo2103@users.noreply.github.com>
2023-06-20 19:38:36 -0400

    nuevos conocimientos

---
+++ Modulo 6/2-S2/rebound/rebound.py
@@ -0,0 +1,41 @@
+# Version de python
+# python -V
+
+#* Version de gestor de paquetes de datos
+# pip -V
+
+#* Instalando virtualenvwrapper
+# pip install virtualenvwrapper
+
+#* Instalando Virtualenv
+# pip install virtualenv
+
+#* Crear entorno virtual
+# virtualenv nombre_entorno
+
+#* Mostrar cuales son los paquetes que tiene instalado el entorno virtual
+
+#& Obtener poliza de seguridad
+#& Get-ExecutionPolicy
+
+#& Desactivar la poliza de seguridad
+#& Set-ExecutionPolicy RemoteSigned
+
+##~ Primero hay que activar el entorno virtual
+##~ --powershell
+##~ .\django_env\Scripts\active.ps1
+##~ --cmd
+##~ django_env\Scripts\activate.ps1
+
+#* pip list
+#~ Para guardar la lista en un archivo txt
+#~ pip list > lista.txt
+
+#* Instalar Django en el entorno virtual creado
+# pip installa django
+
+#* Verificar que version de Django se instalo
+# python -m django --version
+
+#* Mostrar cual es el comando de ayuda de pip
+# pip help
